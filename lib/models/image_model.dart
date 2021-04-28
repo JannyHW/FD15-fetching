fd0700b38409ae4b84a1310a18d109cef66b6224
Janny K <JannyKosin@gmail.com>
2021-04-28 13:06:11 -0700

    Update image_model.dart

--- lib/models/image_model.dart
+++ lib/models/image_model.dart
@@ -1,13 +1,22 @@
-//create a model for each image
+//create a model for each image from JSON data
+// {
+// "albumId": 1,
+// "id": 1,
+// "title": "accusamus beatae ad facilis cum similique qui sunt",
+// "url": "https://via.placeholder.com/600/92c952",
+// "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+// },
 
 class ImageModel {
+  //proprety names here, based on property names from JSON above 
   int id;
   String url;
   String title;
 
+  // option 1 to create a new instance
   ImageModel(this.id, this.url, this.title);
 
-  //creating a new ImageModel instance out of JSON
+  //option 2 to create a new ImageModel instance out of JSON
   ImageModel.fromJson(Map<String, dynamic> parsedJson) {
     id = parsedJson['id'];
     url = parsedJson['url'];
